fb384de69a35ee251d95976b32be24d52f8be5b6
Otto <ottoalves22@gmail.com>
2020-07-14 01:47:20 -0300

    Extrair nome do abatador

--- parser.py
+++ parser.py
@@ -6,11 +6,27 @@ x = match('1', ['Otto', 'Loga'], [['Otto', 2], ['Logan', 5]])
 data = x.jsonificado()
 print(data)
 '''
-def newPlayer(entrada):
+def newPlayer(entrada, jogadores): #adiciona um novo jogador que se logou ao conjunto de jogadores da partida
     aux = entrada.split("n\\")[1].split("\\t")[0]
-    return aux
+    if aux in jogadores:
+        return jogadores
+    else:
+        jogadores[aux] = 0
+        return jogadores
+
+def killed(entrada, jogadores):
+    if '<world>' in x: #desconta um abate do jogar morto pelo ambiente
+        morto = entrada.split("killed ")[1].split(" by")[0]
+        jogadores[morto] -= 1
+        return jogadores
+    else:
+        matador = entrada.split(": ")[1].split(" killed")[0] #TODO PRECISO TIRAR O NOME DE QUEM ABATEU
+        jogadores[matador] += 1
+        return jogadores
+        print((x)) #contabilizar abate pro primeiro nome
 
 game = 0
+abates = 0
 jogadores = {}
 
 f = open("games.log", "r")
@@ -18,18 +34,16 @@ for x in f:
     if 'InitGame' in x:
         game += 1
     if 'ClientUserinfoChanged' in x:
-        player = newPlayer(x)
-        if player in jogadores:
-            pass
-        else:
-            jogadores[player] = 0
-    #if 'Shutdown' in x:
-    #    break
+        jogadores = newPlayer(x, jogadores)
+
     '''
     if 'killed' in x: # criar funcao para medir abates
-        if '<world>' in x:
+        abates += 1
+        if '<world>' in x: #juntar abates em uma funcao
             pass #descontar 1 abate do nome que morreu para <world>
         else:
             print((x)) #contabilizar abate pro primeiro nome
-'''
+    '''
+    #if 'Shutdown' in x: #partida finalizada, hora de criar estatisticas e esvaziar dict de jogadores e abates
+    #    break
 print(jogadores)
